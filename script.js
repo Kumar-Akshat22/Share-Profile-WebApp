5aaacce2faafe1beff10757b174f0293f7027cd1
Kumar Akshat <97528193+Kumar-Akshat22@users.noreply.github.com>
2023-12-26 22:54:00 +0530

    Update script.js file

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ function closeModal(){
 
 function shareOnFacebook(){
 
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('https://codehelp-weather-app.netlify.app/'), 'facebook-share-dialog', 'width=626,height=436');
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('https://share-profile-web-app.vercel.app/'), 'facebook-share-dialog', 'width=626,height=436');
 }
 
 function shareOnLinkedIn(){
 
-    window.open('https://www.linkedin.com/shareArticle?url=' + encodeURIComponent('https://codehelp-weather-app.netlify.app/') + '&title=' + encodeURIComponent('Akshat') + '&summary=' + encodeURIComponent('Hello, World!') + '&source=' + encodeURIComponent('Your Source'), 'linkedin-share-dialog', 'width=626,height=436');
+    window.open('https://www.linkedin.com/shareArticle?url=' + encodeURIComponent('https://share-profile-web-app.vercel.app/') + '&title=' + encodeURIComponent('Akshat') + '&summary=' + encodeURIComponent('Hello, World!') + '&source=' + encodeURIComponent('Your Source'), 'linkedin-share-dialog', 'width=626,height=436');
 
 }
 
 function shareOnWhatsapp(){
 
-    window.open('https://api.whatsapp.com/send?text=' + encodeURIComponent('Your Message') + '%20' + encodeURIComponent('https://codehelp-weather-app.netlify.app/'));
+    window.open('https://api.whatsapp.com/send?text=' + encodeURIComponent('Your Message') + '%20' + encodeURIComponent('https://share-profile-web-app.vercel.app/'));
 
 
 }
